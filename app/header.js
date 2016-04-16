26563176a356e4fd76739ed14fc53c3cbb209af2
davyengone <engonenzedavy@yahoo.com>
2016-04-16 23:52:42 +0200

    adding assets and view

---
+++ app/header.js
@@ -0,0 +1,8 @@
+
+const Header = () => {
+ return (<header>
+          <h1>Hackages, Where Hack goes with Education</h1>
+        </header>);
+}
+
+export defaut Header;
---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const App = () => {
+  return (<h1></h1>);
+};
+
+ReactDom.render(<App/>, document.getElementById("hackages"));
+
+export default App;
