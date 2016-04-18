6144d3772feb50fb501d2ac6e4b5742c0bde0482
davyengone <engonenzedavy@yahoo.com>
2016-04-18 10:25:13 +0200

    added timeline

--- app/index.js
+++ app/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import Header from './header.js';
-
+import Timeline from './timeline';
 
 const App = () => {
-  return (<Header/>);
+  return (<div>
+          <Header/>
+          <Timeline/>
+         </div>);
 };
 
 ReactDom.render(<App/>, document.getElementById('hackages'));
---
+++ app/timeline.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Section = () => {
+  return (<section id="cd-timeline" class="cd-container"></section>);
+};
+
+const Timeline = () => {
+
+  return (<Section/>);
+};
+
+
+export default Timeline;
