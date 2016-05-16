39a9f6fbf7d5ccdd39285023390b8c458920b182
davyengone <engonenzedavy@yahoo.com>
2016-05-16 09:33:47 +0200

    first version of timeline is done

--- app/timeline.js
+++ app/timeline.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Events from './events';
 
 const Section = () => {
   return (<section id="cd-timeline" class="cd-container"></section>);
 };
 
 const Timeline = () => {
-
-  return (<Section/>);
+  return (
+    <div>
+     <Events/>
+    </div>
+  );
 };
 
 
