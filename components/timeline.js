a96c82bfe5a8254a617f1a47ca28270988d75620
davyengone <engonenzedavy@yahoo.com>
2016-05-31 22:46:03 +0200

    added timeline and events

---
+++ components/timeline.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Events from './events';
+
+const Section = () => {
+  return (<section id="cd-timeline" className="cd-container"></section>);
+};
+
+const Timeline = () => {
+  return (
+     <Events/>
+  );
+};
+
+
+export default Timeline;
