796d7b5e5c74f3b01f78b88403f355d4c66e9b4d
davyengone <engonenzedavy@yahoo.com>
2016-06-08 21:20:53 +0200

    toggle button for mobile

---
+++ components/toggleButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default () => {
+  return (
+        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-backyard">
+          <span className="sr-only">Toggle navigation</span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+          <span className="icon-bar"></span>
+        </button>
+       );
+};
+
+
