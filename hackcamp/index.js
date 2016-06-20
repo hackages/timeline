729ed76cb5d4dba6c336caf927e6f3d465c7b65d
davyengone <engonenzedavy@yahoo.com>
2016-06-20 22:17:59 +0200

    hackcamp page

---
+++ hackcamp/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import './css/style.css';
+
+const Program = () => {
+ return (
+  <div id='hackcamp'>
+    <ul className="cd-container">
+      <li className="cd-service cd-service-divider"></li>
+		  <li className="cd-service cd-service-1">
+			  <h2>What is a HackCamp?</h2>
+        <p>More than 60 hours of learning, coding and discovery of the latest web technologies.</p>
+        <p>12 hours a day, 6 days in a row. One thing to do, just code, practise and improve your craft.</p>
+        <p>12 hand picked candidates: An interview is required to attend our HackCamp.</p>
+        <p>Build, Test, Reverse-engineering of AngularJS by building core components yourself.</p>
+		  </li>
+      <li className="cd-service cd-service-1">
+			  <h2>A Week of Hack</h2>
+        <p>Day 0: Pre-bootcamp, JavaScript Koans (mandatory)</p>
+        <p>Day 1: Advanded JavaScript concepts, build a library from scratch</p>
+        <p>Day 2: ES2015 or JavaScript Next & Angular 1, core concepts</p>
+        <p>Day 3: Angular 1, directive...</p>
+        <p>Day 4: Angular 1, Rebuild core components(ng-if, ng-repeat)...</p>
+        <p>Day 5: Angular 2, From Angular 1 to Angular 2</p>
+        <p>Day 6: Angular 2, Core concepts</p>
+		  </li>
+      <li className="cd-service cd-service-1">
+			  <h2>Daily Program</h2>
+        <p>Program starts at 8:30am and finishes at 6:30p.m but can stay as late as 10p.m</p>
+        <p>Breakfast: We provide you with breakfast from 8 a.m. It's an unique moment to get to know your peers better.</p>
+        <p>Challenge: Every morning, we reinforce what we learnt the day before with a challenge.</p>
+        <p>Lecture: We introduce you to the main topics of the day with few slides before you start applying them on your project.</p>
+        <p>Live coding: We starts a set of exercises with a live coding session then you'll pick up from there.</p>
+        <p>Pair programming: Every day, you'll be randomly paired with another developer to solve daily issues as a team.</p>
+        <p>Lunch: There are nice places around the lab where you can get food.</p>
+        <p>Mentorship program: Everyday, we invite expert in JavaScript, Angular to share their experience with you.</p>
+        <p>Officially, the session ends at 6.30pm but you can stay as late as 10p.m to dig deeper on few topics.</p>
+		  </li>
+      <li className="cd-service cd-service-2">
+        <h2>Why joining a HackCamp?</h2>
+        <p>A year worth of learnings put into one intense week. You've never experienced something like this before.</p>
+        <p>Advanced curriculum built by a hand picked group of engineers and web experts.</p>
+        <p>Pratical, you will be spending 80% of your time coding, building apps, testing and discovering.</p>
+        <p>In a week you'll cover JavaScript, Angular and all the tools that will help you build very fast applications.</p>
+        <p>The interview process make sure that you'll be among people with good fundamentals in programming.</p>
+        <p>Architect a real world project, build it, test it and deploy it. Learn how to apply every step on your own project.</p>
+        <p></p>
+      </li>
+
+      <li className="cd-service cd-service-3">
+        <h2>Upcoming bootcamp</h2>
+        <p>JavaScript & Angular 1 & Angular 2: Brussels, 25th-30th July 2016</p>
+        <p>This is our last Angular 1 bootcamp</p>
+		  </li>
+
+      <li className="cd-service cd-service-4">
+			  <h2>CMS Integration</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis pariatur tenetur quod veritatis nulla aspernatur architecto! Fugit, reprehenderit amet deserunt molestiae ut libero facere quasi velit perferendis ullam quis necessitatibus!</p>
+      </li>
+
+      <li className="cd-service cd-service-divider"></li>
+    </ul>
+    <footer></footer>
+  </div>
+ )
+};
+
+export default Program;
