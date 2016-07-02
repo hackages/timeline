c7ddfb9cf5e8e349b3d99d3528537c9bb6cd2b3c
davyengone <engonenzedavy@yahoo.com>
2016-07-02 23:57:23 +0200

    server side rendering

--- server/app.js
+++ server/app.js
@@ -1,19 +1,45 @@
 import express from 'express';
-import schema from './data/schema';
+import http from 'http';
 import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import App from '../components/';
+import { renderToString } from 'react-dom/server';
+import { match, RouterContext } from 'react-router';
+import { routes } from './routes';
 
-const server = express();
+const app = express();
 
-const PORT = process.env.PORT || 9000;
+app.use(express.static('public'));
 
-server.get('/', (req, res) => {
-  const html = ReactDOMServer.renderToString(App());
-  res.send(html);
+app.set('view engine', 'ejs');
+
+app.get('*', (req, res) => {
+  // routes is our object of React routes defined above
+  match({ routes, location: req.url }, (err, redirectLocation, props) => {
+    if (err) {
+      // something went badly wrong, so 500 with a message
+      res.status(500).send(err.message);
+    } else if (redirectLocation) {
+      // we matched a ReactRouter redirect, so redirect from the server
+      res.redirect(302, redirectLocation.pathname + redirectLocation.search);
+    } else if (props) {
+      // if we got props, that means we found a valid component to render
+      // for the given route
+      const markup = renderToString(<RouterContext {...props} />);
+
+      // render `index.ejs`, but pass in the markup we want it to display
+      res.render('index', { markup })
+
+    } else {
+      // no route match, so 404. In a real app you might render a custom
+      // 404 view here
+      res.sendStatus(404);
+    }
+  });
 });
 
+const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}`)
+server.listen(3003);
+
+server.on('listening', () => {
+  console.log('Listening on 3003');
 });
